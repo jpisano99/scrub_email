349674da26be6f477a68f5edaf621b82b885377c
jpisano99 <jpisano@cisco.com>
2019-09-12 11:25:44 -0400

    Initial committ

--- my_app/main.py
+++ my_app/main.py
@@ -1,4 +1,7 @@
 from my_app.ss_lib.Ssheet_class import Ssheet
+from my_app.func_lib.push_list_to_xls import push_list_to_xls
+from my_app.func_lib.push_xls_to_ss import push_xls_to_ss
+
 import time
 # Get the SmartSheets we need
 raw_emails = Ssheet('saas_activation_raw_emails')
@@ -10,10 +13,45 @@ saas_data = Ssheet('saas_activation_data')
 # print(raw_sender_col_id, raw_subject_col_id, raw_body_col_id)
 
 
-search_tags = ['Bill To ID:', 'Partner Account Name:', 'End Customer Name:']
+search_tags = ['Bill To ID:',
+               'Partner Account Name:',
+               'End Customer Name:',
+               'Subscription Reference ID:',
+               'CCW Web Order ID:',
+               'Ship To ID:',
+               'Order Contact First Name:',
+               'Order Contact Last Name:',
+               'Order Contact Email:',
+               'Order Contact Phone:',
+               'Billing Contact First Name:',
+               'Billing Contact Last Name:',
+               'Billing Contact Email:',
+               'Billing Contact Phone:',
+               'Reseller Contact First Name:',
+               'Reseller Contact Last Name:',
+               'Reseller Contact Email:',
+               'Reseller Contact Phone:',
+               'Service To Contact First Name:',
+               'Service To Contact Last Name:',
+               'Service To Contact Phone:',
+               'Initial Term:',
+               'Renewal Term:',
+               'Prepayment Term:',
+               'Billing Model:',
+               'MRR:',
+               'Route to Market:',
+               'Service Requested Start Date:']
+
 raw_rows = raw_emails.get_rows()
+scrubbed_list = []
+my_row = []
+for my_col_header in search_tags:
+    my_row.append(my_col_header[:-1])
+scrubbed_list.append(my_row)
+
 for row in raw_rows.items():
     raw_body = row[1]['raw_body']
+    my_row = []
 
     for search_tag in search_tags:
         search_value = ''
@@ -26,15 +64,15 @@ for row in raw_rows.items():
             else:
                 search_value = search_value + search_char
         print(search_tag, search_value)
+        my_row.append(search_value)
+
+    scrubbed_list.append(my_row)
     print()
 
-    #print(raw_body.find('Partner Account Name'))
-    #print(raw_body.find('End Customer Name'))
+push_list_to_xls(scrubbed_list, 'junk.xlsx')
+push_xls_to_ss('junk.xlsx', 'saas_activation_data')
 
 
-    # 'Bill To ID'
-    # 'Partner Account Name'
-    # 'End Customer Name'
 
 
 
